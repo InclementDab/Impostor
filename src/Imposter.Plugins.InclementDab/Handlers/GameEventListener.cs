9c668487d01cbfa11374d0dadcd75f3cc8c243af
InclementDeath <tylerpaul2329@gmail.com>
2020-11-07 09:02:06 -0500

    Update GameEventListener.cs

--- src/Imposter.Plugins.InclementDab/Handlers/GameEventListener.cs
+++ src/Imposter.Plugins.InclementDab/Handlers/GameEventListener.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Impostor.Api.Events;
+using Impostor.Api.Events.Player;
 
 namespace Impostor.Plugins.InclementDab.Handlers
 {
@@ -11,6 +12,23 @@ namespace Impostor.Plugins.InclementDab.Handlers
         public void OnGameCreated(IGameCreatedEvent e)
         {
             Console.WriteLine("Game > created");
+        }
+
+        [EventListener]
+        public void OnPlayerChat(IPlayerChatEvent e)
+        {
+            Console.WriteLine($"Chat message {e.Message}");
+            
+            if (e.Message == "suicide")
+            {
+
+            }
+
+            if (e.Message == "blind")
+            {
+                e.ClientPlayer.Game.Options.CrewLightMod = 0;
+            }
+            
         }
     }
 }
